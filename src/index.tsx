b357bddb057d925e3566b800ef4eca03904267eb
ttuom1nen <ttuominen@outlook.com>
2020-04-22 22:42:18 +0300

    Added redux dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./components/App";
 import { reducers } from "./redux/rootReducer";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
