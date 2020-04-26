9fe05949fdaf230af345601550fb42f6296f2650
ttuom1nen <ttuominen@outlook.com>
2020-04-26 22:15:14 +0300

    Render refactor

--- src/components/Todos/TodoList.tsx
+++ src/components/Todos/TodoList.tsx
@@ -2,12 +2,18 @@ import React, { useEffect } from "react";
 import TodoItem from "./TodoItem";
 import { StoreState } from "../../redux/initialState";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchTodos, deleteTodo } from "../../redux/todos/actions";
-import { Todo } from "../../redux/todos/interfaces";
+import { fetchTodos, deleteTodo } from "../../redux/todos/todosActions";
+import { Todo } from "../../redux/todos/todosInterfaces";
 
 const TodoList: React.FC = () => {
   const dispatch = useDispatch();
-  const todos: Todo[] = useSelector((state: StoreState) => state.todos);
+  const fetching: boolean = useSelector(
+    (state: StoreState) => state.todos.fetching
+  );
+  const todos: Todo[] = useSelector((state: StoreState) => state.todos.data);
+  const invalidated: boolean = useSelector(
+    (state: StoreState) => state.todos.didInvalidate
+  );
 
   useEffect(() => {
     dispatch(fetchTodos());
@@ -17,21 +23,34 @@ const TodoList: React.FC = () => {
   }, [dispatch]);
 
   const onTodoClick = (id: number): void => {
-    console.log(id);
     dispatch(deleteTodo(id));
+  };
+
+  const renderTodosList = () => {
+    return todos.map((todo: Todo) => (
+      <TodoItem
+        key={todo.id}
+        id={todo.id}
+        title={todo.title}
+        completed={todo.completed}
+        onTodoClick={onTodoClick}
+      />
+    ));
+  };
+
+  const renderError = () => {
+    return <p>Fetching failed!</p>;
   };
 
   return (
     <div>
-      {todos.map((todo: Todo) => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          title={todo.title}
-          completed={todo.completed}
-          onTodoClick={onTodoClick}
-        />
-      ))}
+      {fetching ? (
+        <p>Loading...</p>
+      ) : !invalidated ? (
+        renderTodosList()
+      ) : (
+        renderError()
+      )}
     </div>
   );
 };
