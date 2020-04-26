9d51c16440dfa661fff3e2699b983652b2f64bc7
ttuom1nen <ttuominen@outlook.com>
2020-04-26 22:16:05 +0300

    Added users to root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
-import { todosReducer } from "./todos/reducer";
+import { todosReducer } from "./todos/todosReducer";
+import { usersReducer } from "./users/usersReducer";
 import { StoreState } from "./initialState";
 
 export const reducers = combineReducers<StoreState>({
   todos: todosReducer,
+  users: usersReducer,
 });
