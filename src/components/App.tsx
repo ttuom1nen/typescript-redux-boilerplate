00ca39eb92055f4f7b676f05e136b24531cba40d
ttuom1nen <ttuominen@outlook.com>
2020-05-24 21:42:05 +0300

    Add theme selector

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,64 @@
 import React from "react";
 import TodoList from "./Todos/TodoList";
-import { Button } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+import { useDispatch, useSelector } from "react-redux";
+import { StoreState } from "../redux/initialState";
 import { switchTheme } from "../redux/main/mainActions";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: "flex",
+      justifyContent: "center",
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    appContainer: {
+      backgroundColor: theme.palette.secondary.main,
+    },
+  })
+);
 
 const App: React.FC = () => {
-  // const setTheme = useContext(ThemeContext);
+  const localClasses = useStyles();
   const dispatch = useDispatch();
+  const theme: string = useSelector((state: StoreState) => state.theme);
 
-  const setTheme = (themeName: string): void => {
-    dispatch(switchTheme(themeName));
+  const chooseTheme = (event: React.ChangeEvent<{ value: any }>): void => {
+    dispatch(switchTheme(event.target.value));
+    console.log(event.target.value);
   };
 
   return (
-    <div>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setTheme("lightTheme")}
-      >
-        Set Light Theme
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={() => setTheme("darkTheme")}
-      >
-        Set Dark Theme
-      </Button>
-      <TodoList />
+    <div className={localClasses.appContainer}>
+      <div>
+        <FormControl className={localClasses.formControl}>
+          <InputLabel id="demo-simple-select-label">Theme</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={theme}
+            onChange={chooseTheme}
+          >
+            <MenuItem value={"pinkTheme"}>Pink</MenuItem>
+            <MenuItem value={"lightTheme"}>Light</MenuItem>
+            <MenuItem value={"darkTheme"}>Dark</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className={localClasses.container}>
+        <TodoList />
+      </div>
     </div>
   );
 };
