ef326beee239637f39e49c4a8104ed3f82015ac4
ttuom1nen <ttuominen@outlook.com>
2020-05-24 21:41:37 +0300

    Fix invalid use of Typography

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import thunk from "redux-thunk";
 import App from "./components/App";
 import { reducers } from "./redux/rootReducer";
 import ThemeProvider from "./components/ThemeProvider";
-import { Typography } from "@material-ui/core";
 
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -16,9 +15,7 @@ const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider>
-      <Typography color="textPrimary">
-        <App />
-      </Typography>
+      <App />
     </ThemeProvider>
   </Provider>,
   document.querySelector("#root")
